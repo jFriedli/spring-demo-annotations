b6df504d5a2366019a9e8a328d137809072043d0
Jonas friedli <jonas.friedli@ch.ibm.com>
2019-03-06 14:41:06 +0100

    init

---
+++ src/main/java/ch/jfriedli/springdemo/springdemoannotation/App.java
@@ -0,0 +1,13 @@
+package ch.jfriedli.springdemo.springdemoannotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
