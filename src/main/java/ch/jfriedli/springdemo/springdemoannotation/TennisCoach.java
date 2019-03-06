60cba824371eb2bd5f7f541d7fa84dc613d617f4
jFriedli <jonas.friedli@ch.ibm.com>
2019-03-06 15:26:49 +0100

    add field injection

--- src/main/java/ch/jfriedli/springdemo/springdemoannotation/TennisCoach.java
+++ src/main/java/ch/jfriedli/springdemo/springdemoannotation/TennisCoach.java
@@ -8,13 +8,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	
+	@Autowired
 	private FortuneService fortuneService;
 	
+	/*
 	@Autowired
 	public TennisCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
-
+	*/
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice backhand";
