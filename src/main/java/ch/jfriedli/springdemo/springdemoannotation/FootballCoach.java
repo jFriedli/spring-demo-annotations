1e92868ea94cfceb3095d10eea75d65720c93438
jFriedli <jonas.friedli@hotmail.com>
2019-03-07 11:38:28 +0100

    play around with PostConstruct and PreDestroy annotations

--- src/main/java/ch/jfriedli/springdemo/springdemoannotation/FootballCoach.java
+++ src/main/java/ch/jfriedli/springdemo/springdemoannotation/FootballCoach.java
@@ -2,9 +2,11 @@ package ch.jfriedli.springdemo.springdemoannotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class FootballCoach implements Coach {
 	
 	private FortuneService fortuneService;
--- src/main/java/ch/jfriedli/springdemo/springdemoannotation/TennisCoach.java
+++ src/main/java/ch/jfriedli/springdemo/springdemoannotation/TennisCoach.java
@@ -1,14 +1,17 @@
 package ch.jfriedli.springdemo.springdemoannotation;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 
 //default bean ID is the class name starting with lower case
 //tennisCoach
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	@Autowired
@@ -36,7 +39,17 @@ public class TennisCoach implements Coach {
 	@Autowired
 	@Qualifier("randomFortuneService")
 	public void doSomeCrazyStuff(FortuneService fortuneService) {
-		System.out.println("TennisCoach: Do crazy stuff!");
+		//System.out.println("TennisCoach: Do crazy stuff!");
+	}
+	
+	@PostConstruct
+	public void postContruct() {
+		System.out.println("PostConstruct");
+	}
+	
+	@PreDestroy
+	public void preDestroy() {
+		System.out.println("PreDestroy");
 	}
 
 }
