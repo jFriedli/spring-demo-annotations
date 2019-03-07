50ceed8f9ad3bd9c95cba7ac2ac4f68170dbb71d
jFriedli <jonas.friedli@hotmail.com>
2019-03-07 11:43:53 +0100

    add java configuration class

---
+++ src/main/java/ch/jfriedli/springdemo/springdemoannotation/SportConfig.java
@@ -0,0 +1,12 @@
+package ch.jfriedli.springdemo.springdemoannotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ch.jfriedli.springdemo")
+public class SportConfig {
+	
+	
+
+}
