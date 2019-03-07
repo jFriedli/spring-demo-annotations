977f5b537de84e296ebc13f0a70c2df877367fab
jFriedli <jonas.friedli@hotmail.com>
2019-03-07 13:34:37 +0100

    add value injection from file

--- src/main/java/ch/jfriedli/springdemo/springdemoannotation/SportConfig.java
+++ src/main/java/ch/jfriedli/springdemo/springdemoannotation/SportConfig.java
@@ -3,9 +3,11 @@ package ch.jfriedli.springdemo.springdemoannotation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("ch.jfriedli.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 	
 	// define bean for sad fortune service
--- src/main/java/ch/jfriedli/springdemo/springdemoannotation/SwimCoach.java
+++ src/main/java/ch/jfriedli/springdemo/springdemoannotation/SwimCoach.java
@@ -1,12 +1,21 @@
 package ch.jfriedli.springdemo.springdemoannotation;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	@Value("${coach.email}")
+	private String email;
+	
+	@Value("${coach.team}")
+	private String team;
+	
 	public SwimCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Swim 1k";
@@ -16,5 +25,15 @@ public class SwimCoach implements Coach {
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
+	
+	
 
 }
